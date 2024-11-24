ab65899c657dd86a987cb6c8698f00719e6188d7
Gilgal123 <128999981+Gilgal123@users.noreply.github.com>
2024-11-24 14:03:59 +0200

    [ex1][part2]: modify part 2
    Signed-off-by: Gilgal123 <128999981+Gilgal123@users.noreply.github.com>

--- Lab1/part2/worker.js
+++ Lab1/part2/worker.js
@@ -1,5 +1,7 @@
 // Duration of your trace, in milliseconds
-let TRACE_LENGTH;
+let Cache_line_elem = 8;
+let TRACE_LENGTH = 2621440; // 20MB/64B
+let ARR_SIZE = 100;
 
 // Array of length TRACE_LENGTH with your trace's values
 let T;
@@ -9,18 +11,38 @@ let start;
 
 function record() {
   // Create empty array for saving trace values
+  let P = 500;
+  let N = 327680;
   T = new Array(TRACE_LENGTH);
+  let result = new Array(ARR_SIZE);
 
   // Fill array with -1 so we can be sure memory is allocated
+  result.fill(0, 0, result.length);
   T.fill(-1, 0, T.length);
 
+
   // Save start timestamp
-  start = performance.now();
+  let count = 0 ;
+  let end;
+  for (let j = 0; j < ARR_SIZE; ++j) {
+    count = 0;
+    start = performance.now();
+    end = start;
+    while ( (end - start) < P) {
+      for (i=0; i < N; ++i) {
+        let tmp = T[i*Cache_line_elem];
+      }
+      count++;
+      end = performance.now();
+    }
+    result[j] = count;
+  }
+  console.log(result);
 
   // TODO (Exercise 2-2): Record data for TRACE_LENGTH seconds and save values to T.
 
   // Once done recording, send result to main thread
-  postMessage(JSON.stringify(T));
+  postMessage(JSON.stringify(result));
 }
 
 // DO NOT MODIFY BELOW THIS LINE -- PROVIDED BY COURSE STAFF
