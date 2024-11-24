94669987ef701c8164665cdf04599b29c2bde951
Gilgal123 <128999981+Gilgal123@users.noreply.github.com>
2024-11-24 11:05:23 +0200

    [ex1][part1]: Modify warmup.js
    Signed-off-by: Gilgal123 <128999981+Gilgal123@users.noreply.github.com>

--- Lab1/part1/warmup.js
+++ Lab1/part1/warmup.js
@@ -1,9 +1,9 @@
 const runs = 10;
+const N = 100000;
 
 function measureOneLine() {
   const LINE_SIZE = 16; // 64/sizeof(int)
   let result = [];
-
   // Fill with -1 to ensure allocation
   const M = new Array(runs * LINE_SIZE).fill(-1);
 
@@ -20,9 +20,18 @@ function measureOneLine() {
 
 function measureNLines() {
   let result = [];
+  const LINE_SIZE = 16; // 64/sizeof(int)
+  let array_size = 10000 * LINE_SIZE;
+  const M = new Array(array_size).fill(-1);
 
-  // TODO: Exercise 1-1
-
+  for (let j = 0; j < 10; j++) {
+    const start = performance.now();
+    for (let i = 0; i < N; i++) {
+      let val = M[(j*(LINE_SIZE* N) + i * LINE_SIZE) % (array_size)];
+    }
+    const end = performance.now();
+    result.push(end - start);
+  }
   return result;
 }
 
