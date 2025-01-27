5d3990c42b1cdb5a489a572510656b856a3216ac
Royzou1 <royeezo@gmail.com>
2025-01-27 13:33:25 +0200

    koke

--- Lab3/SpectreLab/part3-src/attacker-part3.c
+++ Lab3/SpectreLab/part3-src/attacker-part3.c
@@ -81,6 +81,8 @@ int run_attacker(int kernel_fd, char *shared_memory) {
             for (size_t i = 0; i < 17000 ; i++) //fool BP -in part 2.4 we need to change max(i) //rand max i?
             { 
                 mfence();
+                flush_cache();
+                mfence();
                 call_kernel_part3(kernel_fd, shared_memory, rand() % 2);
             }
             mfence();
