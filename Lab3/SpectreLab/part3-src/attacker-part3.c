2dc51b969818982d0f9f1e4909cc35db12aafdac
Royzou1 <royeezo@gmail.com>
2025-01-27 13:09:11 +0200

    koke

--- Lab3/SpectreLab/part3-src/attacker-part3.c
+++ Lab3/SpectreLab/part3-src/attacker-part3.c
@@ -62,11 +62,11 @@ int run_attacker(int kernel_fd, char *shared_memory) {
 
     for (current_offset = 0; current_offset < SHD_SPECTRE_LAB_SECRET_MAX_LEN; current_offset++) {
         int hist[256] = {0};
-        for (int i = 0; i < 50; i++)
+        for (int i = 0; i < 30; i++)
         {
             char leaked_byte;
             int min = 100000;
-            for (size_t i = 0; i < 640 ; i++) //fool BP -in part 2.4 we need to change max(i) //rand max i?
+            for (size_t i = 0; i < 10000 ; i++) //fool BP -in part 2.4 we need to change max(i) //rand max i?
             { 
                 clean_shared_memory_from_tlb(shared_memory);
                 call_kernel_part3(kernel_fd, shared_memory, rand() % 2);
